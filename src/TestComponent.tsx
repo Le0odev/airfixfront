b1f06e098b635fca8afdad4b5e471d017ce13941
Leonardo Vinicius <leonardovinicius09@hotmail.com>
2024-10-28 20:17:25 -0300

    First# Please enter the commit message for your changes. Lines starting

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const TestComponent = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-blue-100">
-      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSSSS!</h1>
+      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
     </div>
   );
 };
